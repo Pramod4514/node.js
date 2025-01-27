1e46ec70c66107f228a630cce29b67d2ae9c1605
Pramod4514 <118909351+Pramod4514@users.noreply.github.com>
2025-01-27 09:38:37 +0530

    Delete 1_nodeserver directory

--- 1_nodeserver/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var server=http.createServer(function(request,response){
-    response.write('<html><body><h1>Hello welcome to nodejs</h1></body></html>');
-})
-
-server.listen(3030);
-console.log('My server started on port 3030')
